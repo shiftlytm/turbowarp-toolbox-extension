e92d099c0d5c9edbf441d6e0daac1c5e6301c955
shiftlytm <ruslangamidov201504@gmail.com>
2026-06-05 13:30:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ class ToolBox {
     return {
       id: 'toolboxtm',
       name: 'Расширенные возможности',
+      
       blocks: [
         {
           opcode: 'join',
           blockType: Scratch.BlockType.REPORTER,
           text: 'объединить текст [ONE] и [TWO]',
+          
           arguments: {
             ONE: {
               type: Scratch.ArgumentType.STRING
             },
             TWO: {
+              type: Scratch.ArgumentType.STRING
+            }
+          }
+        }
+        {
+          opcode: 'encode',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'закодировать текст [ONE] по Base64',
+          
+          arguments: {
+            ONE: {
               type: Scratch.ArgumentType.STRING
             }
           }
@@ -23,6 +36,10 @@ class ToolBox {
 
   join(args) {
     return args.ONE + args.TWO;
+  }
+
+  encode(args) {
+    return btoa(args.ONE);
   }
 }
 
